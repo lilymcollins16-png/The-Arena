1139b445674c8c5450ce802b8e10142d46c337fb
Lily Collins <lily.m.collins.16@gmail.com>
2026-04-13 21:09:42 -0400

    site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'THE ARENA';
-export const SITE_DESCRIPTION = 'Draw your swords, gladiators; welcome to THE ARENA.';
+export const SITE_TITLE = 'The Arena Journal';
+export const SITE_DESCRIPTION = 'A literary journal run by UVA students in search of truth.';
